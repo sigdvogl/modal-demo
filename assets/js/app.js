eb631d85ff6ce3b1b381b629e4b6df191bc70873
Dominic Vogl <dominic.vogl@sigikid.de>
2019-06-06 16:54:56 +0200

    bugfix slick slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,19 @@
 // prevent mixed $ selections with other scripts
 (function() {
 
-    // Init foundation library for functionality
+    // Init foundation core and plugins for whole functionality
     $(document).foundation();
 
+    // select all slick elements
     $('.js-carousel').each(function() {
+
+        // Init slick instance for each slick element
         $(this).slick(
             {
+                // your settings
                 'dots': true,
-                'arrows': true
+                'arrows': true,
+                'adaptiveHeight': true
             }
         )
     })
